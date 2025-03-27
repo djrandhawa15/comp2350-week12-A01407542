c6b350edb31b688fb6a4c3e1fd24ad0062ee6736
Dilraj <djrandhawa15@hotmail.com>
2025-03-27 11:12:12 -0700

    updt

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,8 @@
 const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
-const databaseName = "lab_example"
-const hostedURI =
-"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databas
-eName+"?retryWrites=true&w=majority"
-const localURI =
-"mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
+const databaseName = "lab_example";
+const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority";
+const localURI ="mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true";
 if (is_hosted) {
 database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
