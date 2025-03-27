a3918ed7fafacd3aa9b42c1d3b2ea23fbd109adc
Dilraj <djrandhawa15@hotmail.com>
2025-03-27 10:40:21 -0700

    updated code

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,11 @@
-const MongoClient = require("mongodb").MongoClient;
-
+const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
-
-
-const hostedURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
-const localURI = "mongodb://localhost/?authSource=admin&retryWrites=true&w=majority;"
-
+const databaseName = "lab_example";
+const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/'lab_example'?retryWrites=true&w=majority";
+const localURI = "mongodb://localhost/'lab_example'?authSource=admin&retryWrites=true";
 if (is_hosted) {
-	var database = new MongoClient(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+	database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+} else {
+	database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
-else {
-	var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-
-module.exports = database;
 		
--- routes/router.js
+++ routes/router.js
@@ -1,16 +1,74 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const database = require('../databaseConnection');
+const User = require('../models/user');
+const Pet = require('../models/pet');
+const Joi = require("joi");
 
+// Home Page - Display all users
 router.get('/', async (req, res) => {
-	console.log("page hit");
-	try {
-		console.log("Test");
-		res.render('index', {message: "This is Awesome!"});
-	}
-	catch(ex) {
-		res.render('error', {message: 'Error'});
-		console.log("Error");
-		console.log(ex);
-	}
+    console.log("Page hit - Home Route");
+    try {
+        const users = await User.find({})
+            .select('first_name last_name email _id')
+            .exec();
+        res.render('index', { allUsers: users });
+    } catch (ex) {
+        res.render('error', { message: 'Error retrieving users' });
+        console.log("Error:", ex);
+    }
+});
+
+// Route to populate initial data
+router.get('/populateData', async (req, res) => {
+    console.log("Populate Data Route");
+    try {
+        let pet1 = new Pet({ name: "Fido" });
+        let pet2 = new Pet({ name: "Rex" });
+
+        await pet1.save();
+        await pet2.save();
+
+        let user = new User({
+            first_name: "Me",
+            last_name: "Awesome",
+            email: "a@b.ca",
+            password_hash: "thisisnotreallyahash",
+            password_salt: "notagreatsalt",
+            pets: [pet1._id, pet2._id]
+        });
+
+        await user.save();
+        console.log("Data populated successfully");
+
+        res.redirect("/");
+    } catch (ex) {
+        res.render('error', { message: 'Error populating data' });
+        console.log("Error:", ex);
+    }
+});
+
+// Route to display pets for a specific user
+router.get('/showPets', async (req, res) => {
+    console.log("Page hit - Show Pets");
+    try {
+        const schema = Joi.string().max(25).required();
+        const validationResult = schema.validate(req.query.id);
+
+        if (validationResult.error) {
+            throw validationResult.error;
+        }
+
+        const userResult = await User.findOne({ _id: req.query.id })
+            .select('first_name _id')
+            .populate('pets')
+            .exec();
+
+        res.render('pet', { userAndPets: userResult });
+    } catch (ex) {
+        res.render('error', { message: 'Error retrieving pets' });
+        console.log("Error:", ex);
+    }
 });
 
 module.exports = router;
