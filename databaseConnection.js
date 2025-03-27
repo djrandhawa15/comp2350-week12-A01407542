967254ac06c7d7fddd4fcabb7b281e1d88151005
Dilraj <djrandhawa15@hotmail.com>
2025-03-27 11:10:27 -0700

    updated name

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,15 @@
 const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
-const databaseName = "lab_example";
-const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/'lab_example'?retryWrites=true&w=majority";
-const localURI = "mongodb://localhost/'lab_example'?authSource=admin&retryWrites=true";
+const databaseName = "lab_example"
+const hostedURI =
+"mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databas
+eName+"?retryWrites=true&w=majority"
+const localURI =
+"mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
 if (is_hosted) {
-	database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
-} else {
-	database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
+database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+}
+else {
+database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
 }
 		
