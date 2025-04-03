c1e36b59c99a0bd090ce935166273cc60743544d
Dilraj <djrandhawa15@hotmail.com>
2025-04-02 19:12:21 -0700

    code

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,14 +1,23 @@
-const database = require("mongoose");
-const is_hosted = process.env.IS_HOSTED || false;
-const databaseName = "lab_example";
-// const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/"+databaseName+"?retryWrites=true&w=majority";
-const hostedURI = "mongodb://theMongoAdmin:accidentalLoginSteps@cluster0-shard-00-00.4ulcc.mongodb.net:27017,cluster0-shard-00-01.4ulcc.mongodb.net:27017,cluster0-shard-00-02.4ulcc.mongodb.net:27017/lab_example?ssl=true&replicaSet=atlas-6brbu-shard-0&authSource=admin&retryWrites=true&w=majority";
+const mongoose = require("mongoose");
 
-const localURI ="mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true";
+const is_hosted = process.env.IS_HOSTED || false;
+
+const databaseName = "lab_example";
+
+const hostedURI = "mongodb://theMongoAdmin:accidentalLoginSteps@cluster0-shard-00-00.6brbu.mongodb.net:27017,cluster0-shard-00-01.6brbu.mongodb.net:27017,cluster0-shard-00-02.6brbu.mongodb.net:27017/" + databaseName + "?ssl=true&authSource=admin&retryWrites=true&w=majority";
+
+const localURI = "mongodb://localhost/" + databaseName + "?authSource=admin&retryWrites=true";
+
 if (is_hosted) {
-database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect(hostedURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+} else {
+    mongoose.connect(localURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 }
-else {
-database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
-		
+
+module.exports = mongoose;
